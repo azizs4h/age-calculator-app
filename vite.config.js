90f07d5673c4ffd3ac0164ef1589ae498771d3b5
Ömer Aziz ŞAHİN <omarazizsah@gmail.com>
2023-04-04 04:06:42 +0300

    env
    feat: env production settings

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,11 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueConfig from './vue.config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/age-calculator-app/',
+  base: vueConfig.publicPath,
   plugins: [vue()],
   resolve: {
     alias: {
