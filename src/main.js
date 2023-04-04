6cc5c9a55c096c75b966e3024b28559c48d62140
Ömer Aziz ŞAHİN <omarazizsah@gmail.com>
2023-04-04 04:11:24 +0300

    Update main.js
    feat: register globally form comp.

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import AInput from '@/components/form/AInput.vue'
-import AButton from '@/components/AButton.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import AButton from '@/components/AButton.vue';
+import AForm from '@/components/form/AForm.vue';
+import AInput from '@/components/form/AInput.vue';
 
-import './assets/scss/main.scss'
+import './assets/scss/main.scss';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.component('a-input', AInput)
-app.component('a-btn', AButton)
-app.use(router)
+app.component('a-form', AForm);
+app.component('a-input', AInput);
+app.component('a-btn', AButton);
+app.use(router);
 
-app.mount('#app')
+app.mount('#app');
