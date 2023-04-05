ca18437c62cfdf252697625c3d89a874166b03af
Ömer Aziz ŞAHİN <omarazizsah@gmail.com>
2023-04-06 01:57:42 +0300

    Update HomeView.vue
    refactor: calculations and key events

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="container">
-    <div class="todo-container">TODO: calculate absolute age <br /></div>
     <div class="age-app-card">
       <a-form ref="formRef">
         <div class="input-section">
@@ -10,6 +9,7 @@
             placeholder="DD"
             type="number"
             :rules="rules.day"
+            @keydown.enter="calculateAge"
           ></a-input>
           <a-input
             v-model="formData.month"
@@ -17,6 +17,7 @@
             placeholder="MM"
             type="number"
             :rules="rules.month"
+            @keydown.enter="calculateAge"
           ></a-input>
           <a-input
             v-model="formData.year"
@@ -24,6 +25,7 @@
             placeholder="YYYY"
             type="number"
             :rules="rules.year"
+            @keydown.enter="calculateAge"
           ></a-input>
         </div>
       </a-form>
@@ -32,25 +34,16 @@
         <a-btn @click="calculateAge"></a-btn>
       </div>
       <div class="calculations-section">
-        <div>
-          <span>{{ calculatedAge.year ?? '--' }}</span>
-          years
-        </div>
-        <div>
-          <span>{{ calculatedAge.month ?? '--' }}</span>
-          months
-        </div>
-        <div>
-          <span>{{ calculatedAge.day ?? '--' }}</span>
-          days
-        </div>
+        <animated-number v-for="(item, index) in calculatedAge" :key="index" :number="item">
+          {{ index }}
+        </animated-number>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref } from 'vue';
 
 const formRef = ref();
 const today = new Date();
@@ -65,25 +58,46 @@ const rules = ref({
 });
 
 const formData = ref({
-  day: day,
-  month: month,
-  year: year
-});
-
-const calculatedAge = ref({
   day: null,
   month: null,
   year: null
 });
 
+const calculatedAge = reactive({
+  years: null,
+  months: null,
+  days: null
+});
+
 const calculateAge = () => {
-  // TODO: calculate absolute age
-  // TODO: create a-form component
   try {
+    /* calculations calculate age with this formula
+      2023 04 06
+      1996 09 04
+      -__________
+      0026 06 02
+    */
+
     if (formRef.value.validate()) {
-      calculatedAge.value.year = year - formData.value.year;
-      calculatedAge.value.month = Math.abs(month - formData.value.month);
-      calculatedAge.value.day = Math.abs(day - formData.value.day);
+      calculatedAge.days = day - formData.value.day;
+      if (calculatedAge.days < 0) {
+        calculatedAge.days = day + 30 - formData.value.day;
+        calculatedAge.months = month - 1 - formData.value.month;
+
+        if (calculatedAge.months < 0) {
+          calculatedAge.months = month + 11 - formData.value.month;
+          calculatedAge.years = year - 1 - formData.value.year;
+        }
+        return;
+      }
+
+      calculatedAge.months = month - formData.value.month;
+      if (calculatedAge.months < 0) {
+        calculatedAge.months = month + 11 - formData.value.month;
+        calculatedAge.years = year - 1 - formData.value.year;
+      } else {
+        calculatedAge.years = year - formData.value.year;
+      }
     }
   } catch (e) {
     console.log('err', e);
@@ -92,79 +106,5 @@ const calculateAge = () => {
 </script>
 
 <style lang="scss">
-@import '@/assets/scss/theme.scss';
-@import '@/assets/scss/typography.scss';
-
-.todo-container {
-  position: fixed;
-  left: 1rem;
-  top: 1rem;
-  background: whitesmoke;
-  border: 4px solid theme-color(purple);
-  border-radius: 0.75rem;
-  padding: 2rem;
-  line-height: 1.5rem;
-}
-
-// TODO: Mobil Responsive layout
-.container {
-  width: 100%;
-  height: 100svh;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.input-section {
-  display: flex;
-  padding-right: 11.5rem;
-  gap: 2rem; // 20px
-
-  .a-input-container {
-    width: calc((100% - 4rem) / 3);
-  }
-}
-
-.horizontal-divider {
-  background-color: theme-color(light-grey);
-  border: none;
-  height: 0.125rem;
-  flex: 1 1 auto;
-}
-
-.age-app-card {
-  background-color: theme-color(white);
-  border-radius: 1.5rem 1.5rem 12rem 1.5rem;
-  padding: 3.5rem; //56px
-  max-width: 840px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-}
-
-.calculate-section {
-  display: flex;
-  align-items: center;
-}
-
-.calculations-section {
-  margin-top: 0.25rem; //4px
-  display: flex;
-  flex-direction: column;
-  gap: 0.625rem; //6px
-
-  div {
-    color: theme-color(off-black);
-    font-size: 6.5rem; //104px
-    font-family: 'Poppins-ExtraBold-Italic';
-    letter-spacing: -0.125rem; //2px
-    display: flex;
-    gap: 0.875rem; //14px
-
-    span {
-      color: theme-color(purple);
-      letter-spacing: -0.25rem; //4px
-    }
-  }
-}
+@import '@/assets/scss/main.scss';
 </style>
